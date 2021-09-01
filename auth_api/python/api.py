2a27a94ddfd500423db3c8e5a296800429569417
Ander <anderplascencia@gmail.com>
2021-09-01 00:34:58 -0500

    login

--- auth_api/python/api.py
+++ auth_api/python/api.py
@@ -1,17 +1,29 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+from flask.helpers import make_response
 from methods import Token, Restricted
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+import hashlib
 
 app = Flask(__name__)
 login = Token()
 protected = Restricted()
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://secret:noPow3r@bootcamp-tht.sre.wize.mx:3306/bootcamp_tht'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db = SQLAlchemy(app)
+users = db.Table('users', db.metadata, autoload=True, autoload_with=db.engine)
+qs = db.session.query(users).all()
+
 
 # Just a health check
 @app.route("/")
 def url_root():
-    return "OK"
+    for row in qs:
+        print(row.username)
+    return "Ok"
 
 
 # Just a health check
@@ -25,10 +37,17 @@ def url_health():
 def url_login():
     username = request.form['username']
     password = request.form['password']
-    res = {
-        "data": login.generate_token(username, password)
-    }
-    return jsonify(res)
+    for row in qs:
+        if username == row.username:
+            salt = f"{password}{row.salt}"
+            hashed=hashlib.sha512(salt.encode('ascii')).hexdigest()
+            if hashed == row.password:
+                role = row.role
+                res = {
+                    "data": login.generate_token(username, role)
+                }
+                return jsonify(res)
+    return make_response("Username/Password combination is incorrect", 403)
 
 
 # # e.g. http://127.0.0.1:8000/protected
@@ -42,4 +61,4 @@ def url_protected():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='localhost', port=8000)
--- auth_api/python/methods.py
+++ auth_api/python/methods.py
@@ -1,11 +1,25 @@
-# These functions need to be implemented
+from flask.json import jsonify
+import jwt
+from flask import jsonify
+
+secret= "my2w7wjd7yXF64FIADfJxNs1oupTGAuW"
+
 class Token:
 
-    def generate_token(self, username, password):
-        return 'test'
+    def generate_token(self, username, role):
+        salt_password = "{password}"
+        token = jwt.encode({
+            "usename": username,
+            "role": role
+            }, key=secret)
+        return token
 
 
 class Restricted:
 
     def access_data(self, authorization):
+        token = authorization[7:-1]
+        print(type(token))
+        data = jwt.decode(token, secret, algorithms=["HS256",])
+        print(data)
         return 'test'
